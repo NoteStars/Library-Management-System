cffd3717d3bbfd8d63092c1593801cbcab4dd593
Jonathan Nguyen <86938788+NoteStars@users.noreply.github.com>
2026-05-01 17:41:02 -0700

    Delete Java directory

--- Java/MyFirstProgram/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String [] args){
-        System.out.println("\\I love pizza");
-        System.out.println("It's really good!");
-        System.out.println();
-        System.out.println();
-    }
-}
-i
